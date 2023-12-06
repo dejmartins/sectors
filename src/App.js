f1056a451b7980df77289e62110bdf919a0746b0
Deji Martins <dejimartins99@gmail.com>
2023-12-06 13:57:16 +0100

    got database dump

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import Home from './pages/Home';
 import React, { useEffect } from 'react';
+// import { database } from './firebase';
+// import { ref, get } from "firebase/database";
 
 function App() {
-  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // const fetchData = async () => {
-    //   const result = await get(ref(database, '/sectors'));
-    //   if (result.exists()) {
-    //     setData([result.val()]);
-    //     console.log(data)
-    //     console.log(result.val())
-    //   } else {
-    //     console.log('No data available');
-    //   }
-    // };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await get(ref(database, '/'));
 
-    // fetchData();
-  }, [])
+  //     if (result.exists()) {
+  //       const jsonData = JSON.stringify(result.val(), null, 2);
+
+  //       const blob = new Blob([jsonData], { type: 'application/json' });
+
+  //       const link = document.createElement('a');
+  //       link.href = URL.createObjectURL(blob);
+  //       link.download = 'database-export.json';
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       document.body.removeChild(link);
+
+  //     } else {
+  //       console.log('No data available');
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [])
 
 
   return (
